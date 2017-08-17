76e002f11773e59d2ca020960b09bacf2e02a999
Kim Carson <kim@brighteyelabs.com>
2017-08-17 07:08:19 -0700

    adding google tracking script

--- cashflow/index.php
+++ cashflow/index.php
@@ -55,6 +55,16 @@ $ethPrice = $priceResult['data']['amount'];
     <script src="https://cdn.datatables.net/responsive/2.1.1/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.1.1/js/responsive.bootstrap.min.js"></script>
     <script src="https://cdn.datatables.net/fixedheader/3.1.2/js/dataTables.fixedHeader.min.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-100571693-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
 
 </head>
 <body>
